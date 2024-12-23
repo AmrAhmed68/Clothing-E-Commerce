b12e21ddd77c5755f007583fe622b14eef8c4578
AmrAhmed68 <amrahmed682003@gmail.com>
2024-12-23 04:11:04 +0200

    Amr

--- frontend/src/components/FilterProduct/CategoryProduct.jsx
+++ frontend/src/components/FilterProduct/CategoryProduct.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductCard from '../ProductCard/ProductCard'
 import useProduct from '../../hooks/useProduct'
 import { useParams } from 'react-router-dom'
@@ -6,10 +6,21 @@ import { useParams } from 'react-router-dom'
 function CategoryProduct() {
     const {categoryName} = useParams()
     const {cproduct , fetchCategory} = useProduct()
+    const [loading , setLoading] = useState([])
     
     useEffect(() => {
+      setLoading(true)
         fetchCategory(categoryName);
+        setLoading(false)
     }, [categoryName]);
+
+    if (loading) {
+      return (
+        <div className="loading-container">
+          <div className="spinner"></div>
+        </div>
+      );
+    }
     
     const Product = Array.isArray(cproduct)
     ? cproduct.map((e) => <ProductCard key={e._id} data={e} />)
--- frontend/src/components/FilterProduct/FilterProduct.jsx
+++ frontend/src/components/FilterProduct/FilterProduct.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductCard from '../ProductCard/ProductCard'
 import useProduct from '../../hooks/useProduct'
 import { useParams } from 'react-router-dom'
@@ -6,10 +6,22 @@ import { useParams } from 'react-router-dom'
 function FilterProduct() {
     const {categoryName, subcategoryName} = useParams()
     const {product , fetchSubCategory} = useProduct()
+    const [loading , setLoading] = useState([])
+    
 
     useEffect(() => {
+      setLoading(true)
         fetchSubCategory(categoryName, subcategoryName);
+        setLoading(false)
         }, [categoryName, subcategoryName]);
+
+        if (loading) {
+          return (
+            <div className="loading-container">
+              <div className="spinner"></div>
+            </div>
+          );
+        }
 
         const Product = Array.isArray(product)
         ? product.map((e) => <ProductCard key={e._id} data={e} />)
