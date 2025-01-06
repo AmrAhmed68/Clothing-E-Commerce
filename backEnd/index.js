a016b669f79fad523df18f79661706101d59c901
AmrAhmed68 <amrahmed682003@gmail.com>
2025-01-06 04:58:28 +0200

    Amr

--- backEnd/index.js
+++ backEnd/index.js
@@ -24,7 +24,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", 'https://clothinge-commerce.vercel.app'); 
+  res.header("Access-Control-Allow-Origin", ['https://clothinge-commerce.vercel.app' , 'http://localhost:4200']); 
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
