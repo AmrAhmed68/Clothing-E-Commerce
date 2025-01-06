82cf71554748aa4e2f5a722007ff0bd199fe5878
AmrAhmed68 <amrahmed682003@gmail.com>
2025-01-06 04:51:42 +0200

    Amr

--- backEnd/index.js
+++ backEnd/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: 'https://clothinge-commerce.vercel.app', 
+    origin: ['https://clothinge-commerce.vercel.app' , 'http://localhost:4200'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
